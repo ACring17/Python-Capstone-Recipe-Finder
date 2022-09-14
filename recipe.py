b3015b626a706e1967dc84c89371e6c26f4c7e0f
Andrew Cring <amcring1@gmail.com>
2022-09-13 17:08:37 -0700

    Adding the additional pages i would like to have on the flask app to help user's search and find their recipes

--- recipe.py
+++ recipe.py
@@ -6,5 +6,18 @@ app =Flask(__name__)
 def index():
     return "<h1>Welcome to Recipe Finder!</h1>"
 
+@app.route('/profile')
+def profile():
+    return "User's profile"
+
+@app.route('/recipe')
+def recipe():
+    return "Search for recipes"
+
+@app.route('/ingredient')
+def ingredient():
+    return "Search for recipes by ingredient"
+
+
 if __name__=='__main__':
     app.run()
