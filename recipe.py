84a8c2382123660a1830df9db72277ad96f92105
Andrew Cring <amcring1@gmail.com>
2022-09-15 13:59:49 -0700

    Added some control flow to the templates and python file

--- recipe.py
+++ recipe.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_logged_in = True
+    return render_template('index.html', user_logged_in=user_logged_in)
 
 @app.route('/profile/<name>')
 def profile(name):
@@ -12,10 +13,12 @@ def profile(name):
 
 @app.route('/recipe/<search>')
 def recipe(search):
+    recipeList = []
     return f"Search for recipes: {search}"
 
 @app.route('/ingredient/<search>')
 def ingredient(search):
+    ingredientList = []
     return f"Search for recipes by ingredient: {search}"
 
 
