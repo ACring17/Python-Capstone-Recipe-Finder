bcaed681d1fb4d7bfa71a9b0df498b61ddfaa87f
Andrew Cring <amcring1@gmail.com>
2022-09-29 09:08:24 -0700

    Setting up the recipe.py file to handle SQLAlchemy

--- recipe.py
+++ recipe.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, render_template, request, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,BooleanField,DateTimeField,RadioField,SelectField,TextAreaField,TextField
 from wtforms.validators import DataRequired
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
