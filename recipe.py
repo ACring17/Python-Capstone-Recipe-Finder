d697b515ba9c2808bc13b1ecdb684bfac6734be6
Andrew Cring <amcring1@gmail.com>
2022-09-29 09:14:30 -0700

    Finished setting up the SQLite db

--- recipe.py
+++ recipe.py
@@ -9,6 +9,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 class InfoForm(FlaskForm):
