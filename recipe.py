9ef66c70f3b248ed02fc2a122b1ce61b0cb24c95
Andrew Cring <amcring1@gmail.com>
2022-09-29 14:49:59 -0700

    Adding comments to help clean up code on recipe.py file

--- recipe.py
+++ recipe.py
@@ -4,23 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,BooleanField,DateTimeField,RadioField,SelectField,TextAreaField,TextField
 from wtforms.validators import DataRequired
-
+# The Path for the db
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-
+# Configuring the db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+# Connecting the SQL
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
-
+# Form set up
 class InfoForm(FlaskForm):
     firstname = StringField("First Name")
     lastname = StringField("Last Name")
     submit = SubmitField("Submit")
 
+# Connecting the templates
 @app.route('/')
 def index():
     user_logged_in = True
@@ -31,7 +32,7 @@ def profile(name):
     firstname = False
     lastname = False
     form = InfoForm()
-
+    # Functionality of the form data
     if form.validate_on_submit():
         firstname = form.firstname.data
         lastname = form.lastname.data
