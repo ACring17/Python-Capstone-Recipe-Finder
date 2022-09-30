8e6ff3c8b4f40d8ae32335687dc2cb0f27f63b82
Andrew Cring <amcring1@gmail.com>
2022-09-30 15:42:35 -0700

    Setting up the files for db migration

--- recipe.py
+++ recipe.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,BooleanField,DateTimeField,RadioField,SelectField,TextAreaField,TextField
 from wtforms.validators import DataRequired
