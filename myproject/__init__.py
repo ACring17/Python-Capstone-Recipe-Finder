400275d831ccf073cd88e2888d3dcd839837fdc1
Andrew Cring <amcring1@gmail.com>
2022-10-03 18:43:02 -0700

    Setting up the db configuration in the init.py file

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,1 +1,15 @@
 # Where the blueprints will be registered
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+basedir =os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
