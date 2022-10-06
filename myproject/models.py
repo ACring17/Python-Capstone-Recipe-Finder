65f949275ce1d6bcea77d67fc18b64ec54f253f9
Andrew Cring <amcring1@gmail.com>
2022-10-06 11:56:00 -0700

    Setting up the user model and user login

--- myproject/models.py
+++ myproject/models.py
@@ -1,19 +1,33 @@
 #Where the models will be housed
-from myproject import db 
+from enum import unique
+from myproject import db,login_manager
+from myproject.core.views import index
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin 
 
-class Users(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class Users(db.Model,UserMixin):
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.Text)
-    password = db.Column(db.Text)
-    name = db.Column(db.Text)
+    username = db.Column(db.String(25), unique=True, index=True)
+    name = db.Column(db.String(65),index=True)
+    password_hash = db.Column(db.String(128))
+
+    rated_recipes = db.relationship('Recipes',backref='rater',lazy=True)
 
     def __init__(self,username,password,name):
         self.username = username
-        self.password = password
+        self.password_hash = generate_password_hash(password)
         self.name = name
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+    
     # String represenation of the db
     def __repr__(self):
         return f"Welcome {self.name} to Recipe Finder"
