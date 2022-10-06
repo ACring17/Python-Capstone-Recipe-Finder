43b658f03ebbd4795ec36c3c79aed5402257cd5e
Andrew Cring <amcring1@gmail.com>
2022-10-05 22:06:14 -0700

    :Connecting the blueprint to the core folder

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,16 +1,18 @@
 # Where the blueprints will be registered
-import os
+# import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecretkey'
-basedir =os.path.abspath(os.path.dirname(__file__))
+from myproject.core.views import core
+app.register_blueprint(core)
+# app.config['SECRET_KEY'] = 'mysecretkey'
+# basedir =os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-Migrate(app,db)
+# db = SQLAlchemy(app)
+# Migrate(app,db)
