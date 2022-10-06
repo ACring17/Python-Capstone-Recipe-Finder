7e39e45ee5c7ba285cc75d6027d394c07aef917c
Andrew Cring <amcring1@gmail.com>
2022-10-06 11:36:44 -0700

    setting up the blueprint to setup the db and login functions in my init.py file

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,7 +1,27 @@
 # Where the blueprints will be registered
+import os
 from flask import Flask
+import flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+## Database Setup ##
+basedir = os.path.abspath(os.path.dirname(__file))
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+## Login Configs ##
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
 
 from myproject.core.views import core
 from myproject.error_pages.handlers import error_pages
