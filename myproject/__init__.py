b20eb37ebc83121cd9be01ace776572b0f25fbba
Andrew Cring <amcring1@gmail.com>
2022-10-05 22:20:06 -0700

    Cleaned up the init.py file so there is less clutter and connect the blueprints for the 403 error page.

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,18 +1,10 @@
 # Where the blueprints will be registered
-# import os
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 from myproject.core.views import core
+from myproject.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
-# app.config['SECRET_KEY'] = 'mysecretkey'
-# basedir =os.path.abspath(os.path.dirname(__file__))
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-# Migrate(app,db)
+app.register_blueprint(error_pages)
