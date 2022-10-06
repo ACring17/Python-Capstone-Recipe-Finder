d6617f655f81af7909e027e3d8ea0d9ffc11c3a9
Andrew Cring <amcring1@gmail.com>
2022-10-06 14:05:24 -0700

    Creating the user login and registration form

---
+++ myproject/users/forms.py
@@ -0,0 +1,35 @@
+import email
+from tokenize import String
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import EqualTo
+from wtforms import ValidationError
+
+from flask_login import current_user
+from myproject.models import Users
+
+class LoginForm(FlaskForm):
+    username = String('username',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Log In') 
+
+class RegistrationForm(FlaskForm):
+    username = String('username',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired(), EqualTo('pass_confirm', message="Passwords must match!")])
+    pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
+    submit = SubmitField('Register!')
+
+    def check_username(self,field):
+        if Users.query.filter_by(username=field.data).first():
+            raise ValidationError('Your Username has been registered already!')
+
+    
+class UpdateUserForm(FlaskForm):
+
+    username = String('username',validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+    def check_username(self,field):
+        if Users.query.filter_by(username=field.data).first():
+            raise ValidationError('Your Username has been registered already!')
