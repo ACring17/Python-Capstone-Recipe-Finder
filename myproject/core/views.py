159ffa3055939ba06cddb79834164fed576c2c88
Andrew Cring <amcring1@gmail.com>
2022-10-15 11:54:11 -0700

    Fixed all current bugs

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -6,7 +6,7 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page',1,type=int)
-    review=Rating.query.order_by(Rating.desc()).paginate(page=page,per_page=20)
+    review=Rating.query.order_by(Rating.rating_id.desc()).paginate(page=page,per_page=20)
     return render_template('index.html',review=review)
 
 @core.route('/recipes')
