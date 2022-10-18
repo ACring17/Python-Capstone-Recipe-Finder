84686e35beb1e9be1afc2fa45451830591ddc9ec
Andrew Cring <amcring1@gmail.com>
2022-10-17 19:23:16 -0700

    Adding a results page for search results, updated the index page to have the search bar, added a forms.py to core to implement a search bar and updated views.py to handle it

---
+++ myproject/core/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo
+from wtforms import ValidationError
+
+from myproject.models import Recipes, Ingredient
+
+class SearchForm(FlaskForm):
+    choices = [('Recipe', 'Recipe'),
+               ('Ingredient', 'Ingredient')]
+    select = SelectField('Search for recipe by:', choices=choices)
+    search = StringField('')
