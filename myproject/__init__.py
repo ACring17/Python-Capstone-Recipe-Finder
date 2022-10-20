b405f3d620c49dbdc066c9f99d3af95fdc1af4bd
Andrew Cring <amcring1@gmail.com>
2022-10-19 19:51:38 -0700

    Regsitered the blueprint for the ratings views and forms

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -28,9 +28,11 @@ login_manager.login_view = 'users.login'
 from myproject.core.views import core
 from myproject.users.views import users
 from myproject.recipes.views import recipes
+from myproject.ratings.views import ratings
 from myproject.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
 app.register_blueprint(users)
 app.register_blueprint(recipes)
+app.register_blueprint(ratings)
 app.register_blueprint(error_pages)
