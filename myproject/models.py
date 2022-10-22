df0fc61e88a0da2a99b17bcec08bd6f8d8eea88b
Andrew Cring <amcring1@gmail.com>
2022-10-21 17:20:52 -0700

    Think I found a solution to the seeding issue

--- myproject/models.py
+++ myproject/models.py
@@ -114,12 +114,39 @@ class Measurement(db.Model):
 
 db.create_all()
 
-#Seeding script
-# import json
+#Seeding script ideas
 
+#**** THINK THIS MIGHT BE THE ANSWER ******
+# from flask.ext.sqlalchemy import SQLAlchemy
+# from sqlalchemy.ext import mutable
+# from flask import Flask
+
+# app = Flask(__name__, template_folder="../templates")
+# db = SQLAlchemy(app, session_options={'autocommit': True})
+
+# class JsonEncodedDict(db.TypeDecorator):
+#     """Enables JSON storage by encoding and decoding on the fly."""
+#     impl = db.Text
+
+#     def process_bind_param(self, value, dialect):
+#         if value is None:
+#             return '{}'
+#         else:
+#             return json.dumps(value)
+
+#     def process_result_value(self, value, dialect):
+#         if value is None:
+#             return {}
+#         else:
+#             return json.loads(value)
+
+
+# mutable.MutableDict.associate_with(JsonEncodedDict)
 
 #***** FEELING GOOD ABOUT THIS METHOD   ****************
-#@recie.route('/', methods=['GET'])
+# import json
+
+#@recie.route('/', methods=['GET', 'POST'])
 #def seed():
     #if request.method == 'GET':
         #file = request.seed['seed']   
