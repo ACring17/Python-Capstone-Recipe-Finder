0e28629a21fb40e428e6e91b2f60da55301c5aa1
Andrew Cring <amcring1@gmail.com>
2022-12-01 10:00:09 -0700

    Removing unused comments to clean up code

--- myproject/ratings/forms.py
+++ myproject/ratings/forms.py
@@ -5,20 +5,13 @@ from wtforms import ValidationError
 
 from myproject.models import Rating
 
-# class SearchRecipe(FlaskForm):
-#     title = StringField('What recipe are you looking for?')
-#     search = SubmitField('Search')
-#     # Complete the search form --- May not need it here in ratings *****
-
 class AddRatingForm (FlaskForm):
 
     rating = IntegerField('What would you rate this dish 1 - 5?', validators=[DataRequired()]) 
     review = TextAreaField('What did you think of this meal?',validators=[DataRequired()])
     submit = submit = SubmitField('Create Review')
 
-    # def check_recipe(self,field):
-    #     if Recipes.query.filter_by(name=field.data).first():
-    #         raise ValidationError('Your recipe has been registered already!')
+    
 
 class UpdateRatingForm(FlaskForm):
 
@@ -27,13 +20,3 @@ class UpdateRatingForm(FlaskForm):
     submit = submit = SubmitField('Update')
 
 
-# class DelRatingForm(FlaskForm):
-
-#     user_id = StringField('',validators=[DataRequired()])
-#     submit = SubmitField('Remove Rating')
-
-#     def check_recipe(self,field):
-#         if Rating.query.filter_by(name=field.data).first():
-#             raise ValidationError('Your rating has been removed!')
-
-#This maybe just a button instead
